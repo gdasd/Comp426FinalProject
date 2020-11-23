e2d6a1e7e5e76105279149cca8b7da43ebf645d0
gdasd <gdas@live.unc.edu>
2020-11-23 16:43:54 -0500

    Account Page partial

---
+++ account.js
@@ -0,0 +1,85 @@
+let handleChangeButtonPress = async function (event) {
+    let newfield = $(`<div class="field password-field"><label class="label">Enter Previous Password</label>
+                    <input class="input" type="password" placeholder="password" name="password" id="input-password">
+                        <label class="label">Enter New Password</label>
+                    <input class="input" type="password" placeholder="password" name="password" id="input-password-new"></input>
+                    </div>`);
+    $('.password-field').replaceWith(newfield);
+    let newbuttons = $(`<div class="password-change">
+    <button class="button-confirm-change">Confirm Change</button>
+    <button class="button-cancel">Cancel Change</button> 
+    <div class="message"></div>
+</div>`);
+    $('.password-change').replaceWith(newbuttons);
+};
+
+let handleCancelButtonPress = async function (event) {
+    let newfield = $(`<div class="field password-field">
+    <label class="label">Password</label>
+    <input class="input" type="password" placeholder="password" name="password" id="input-password" disabled>
+</div>`);
+    $('.password-field').replaceWith(newfield);
+    let newbuttons = $(`<div class="password-change">
+    <button class="button-change">Change Password</button>
+    <div class="message"></div>
+</div>`);
+    $('.password-change').replaceWith(newbuttons);
+};
+
+let handleTrueChangeButtonPress = async function (event) {
+    let passwordnew = $('#input-password-new').val();
+    let password = $('#input-password').val();
+    let username = $('#input-username').val();
+    console.log(username);
+    console.log(password);
+    if (passwordnew == null || password == null || passwordnew == "" || password == "") {
+        $('.message').replaceWith($('<div class="message"><h3>Invalid field! Please fill out both fields</h3></div>'));
+        return;
+    }
+    try {
+    let result = await axios({
+        method: 'put',
+        url: `http://localhost:3030/user/${username}`,
+      //  withCredentials: true,
+        data: {
+         currentPassword: password, 
+         password: passwordnew,
+        },
+      });
+    handleCancelButtonPress(event);
+    $('.message').replaceWith($('<div class="message"><h3>Congrats! Your password has been changed!</h3></div>'));
+    } catch(err) {
+        $('.message').replaceWith($('<div class="message"><h3>Error! Your password could not be changed at this time!</h3></div>'));
+    }
+};
+
+let handleDeleteButtonPress = async function (event) {
+    if (document.getElementById("delete").checked) {
+        let username = $('#input-username').val();
+        try {
+            let s = await axios({
+                method: 'delete',
+                url: `http://localhost:3030/user/${username}`,
+              //  withCredentials: true,
+              });
+            window.location.href = "/index.html";
+            } catch(err) {
+                $('.message').replaceWith($('<div class="message"><h3>Unable to be deleted</h3></div>'));
+            }
+    }
+};
+
+$(async function() {    
+    $('.account').on('click', '.button-change', (e) => {
+        handleChangeButtonPress(e);
+    });
+    $('.account').on('click', '.button-cancel', (e) => {
+        handleCancelButtonPress(e);
+    });
+    $('.account').on('click', '.button-confirm-change', (e) => {
+        handleTrueChangeButtonPress(e);
+    });
+    $('.button-delete').on('click', (e) => {
+        handleDeleteButtonPress(e);
+    });
+});
